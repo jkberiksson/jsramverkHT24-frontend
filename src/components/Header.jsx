c11497531452009d79d49c946fb278948a04ccb1
Jakob Eriksson <jkberiksson@gmail.com>
2024-09-18 16:23:30 +0200

    Change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header>
             <h1 className='text-3xl font-bold underline text-center'>
-                Hello Vite!
+                Hello Vite!!!!
             </h1>
         </header>
     );
