8267fbf63a9d5a6a3f044e21e60b6c5a27792d29
Patrik Arvius <patrik.arvius@gmail.com>
2024-09-26 09:57:36 +0200

    Add additional document tests using a mocked API fetch

--- tests/components/Document.test.jsx
+++ tests/components/Document.test.jsx
@@ -1,37 +1,58 @@
-import { it, expect, describe } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { it, expect, describe, vi, beforeAll, afterEach } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 import Document from '../../src/components/Document.jsx';
 import { MemoryRouter } from 'react-router-dom';
 
 // Mock the fetch API
-/* global.fetch = vi.fn(() =>
-    Promise.resolve({
-        ok: true,
-        json: () =>
-            Promise.resolve({ _id: 1, title: 'title1', content: 'content1' }),
-    })
-); */
+beforeAll(() => {
+    window.fetch = vi.fn(() =>
+        Promise.resolve({
+            ok: true,
+            json: () =>
+                Promise.resolve({ _id: 1, title: 'title1', content: 'content1' }),
+        })
+    );
+});
+
+afterEach(() => {
+    cleanup();
+})
 
 describe('Document test', () => {
-    render(
-        <MemoryRouter>
-            <Document />
-        </MemoryRouter>
-    );
+    it('Should have correct input and textarea values and be in the document', async () => {
+        render(
+            <MemoryRouter>
+                <Document />
+            </MemoryRouter>
+        );
+        const textbox = await screen.findAllByRole('textbox');
+        const content = screen.getByText(/content1/i);
+        const title = screen.getByDisplayValue(/title1/i);
 
-    /* it('Should have correct display value and be in the document', () => {
-        const input = screen.getByRole('input');
-        expect(input).toBeInTheDocument();
-    }); */
+        expect(textbox[0]).toBeInTheDocument();
+        expect(textbox[1]).toBeInTheDocument();
+        expect(content).toBeInTheDocument();
+        expect(title).toBeInTheDocument();
+    });
 
     it('Should have link going back to homepage', () => {
+        render(
+            <MemoryRouter>
+                <Document />
+            </MemoryRouter>
+        );
         const linkElement = screen.getByRole('link');
         expect(linkElement).toHaveTextContent('Back');
         expect(linkElement).toHaveAttribute('href', '/');
     });
 
     it('Should have save button', () => {
+        render(
+            <MemoryRouter>
+                <Document />
+            </MemoryRouter>
+        );
         const saveButton = screen.getByRole('button');
         expect(saveButton).toHaveTextContent('Save');
         expect(saveButton).toBeInTheDocument();
